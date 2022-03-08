e6beadeb110597e23d1a4a65167860573bc0ec32
Kara Alexandra <ardnaxelarak@gmail.com>
2022-03-07 16:31:04 -0800

    Add basic deck functionality

--- game.js
+++ game.js
@@ -5,17 +5,28 @@ const Terrain = {
   Wall: {value: 1, chr: '#', light: false, enter: false},
   Room: {value: 2, chr: '.', light: true, enter: true},
   Corridor: {value: 3, chr: '.', light: true, enter: true},
-  Stair: {value: 3, chr: '>', light: true, enter: true, descend: true},
-  Door: {value: 4, chr: '+', light: true, enter: true},
+  Stair: {value: 4, chr: '>', light: true, enter: true, descend: true},
+  Door: {value: 5, chr: '+', light: true, enter: true},
+};
+
+const Magic = {
+  Red: {color: "#F66", symbol: "!"},
+  Green: {color: "#0F0", symbol: "+"},
+  Blue: {color: "#88F", symbol: "*"},
+  Yellow: {color: "#FF0", symbol: "="},
+
+  types: function() {
+    return [this.Red, this.Green, this.Blue, this.Yellow];
+  },
 };
 
 const Game = {
   width: 50,
-  height: 33,
+  height: 30,
   display: null,
   map: [],
   fov: null,
-  player: {x: 0, y: 0},
+  player: {hp: 5, hp_max: 10},
 
   init: function() {
     this.display = new ROT.Display({width: 80, height: 35, fontSize: 15, spacing: 1.1});
@@ -24,6 +35,7 @@ const Game = {
     document.body.addEventListener("keypress", this._keypress.bind(this));
     this.fov = new ROT.FOV.PreciseShadowcasting(this._lightPasses.bind(this));
     this._generateMap();
+    this._initDeck();
     this._updateState();
   },
 
@@ -89,6 +101,31 @@ const Game = {
     }
   },
 
+  _initDeck: function() {
+    this.player.deck = [];
+    this.player.discard = [];
+    this.player.hand = [];
+    for (const type of Magic.types()) {
+      for (var i = 0; i < 4; i++) {
+        this.player.deck.push({type: type, value: 1});
+      }
+    }
+    this.player.deck = ROT.RNG.shuffle(this.player.deck);
+  },
+
+  _fillHand: function() {
+    while (this.player.hand.length < 4) {
+      if (this.player.deck.length == 0) {
+        if (this.player.discard.length == 0) {
+          return;
+        }
+        this.player.deck = ROT.RNG.shuffle(this.player.discard);
+        this.player.discard = [];
+      }
+      this.player.hand.push(this.player.deck.shift());
+    }
+  },
+
   _keypress: function(e) {
     const ch = String.fromCharCode(e.charCode);
     switch (ch) {
@@ -115,6 +152,21 @@ const Game = {
     this.fov.compute(this.player.x, this.player.y, 7, callback.bind(this));
 
     this._drawMap();
+
+    this.display.drawText(2, 32, "Player the Explorer");
+    this.display.drawText(2, 33, `HP: ${this.player.hp.toString().padStart(this.player.hp_max.toString().length)} / ${this.player.hp_max}`);
+
+    this._fillHand();
+    this.display.drawText(54, 1, "Hand:");
+    this.display.drawText(54, 2, " ".repeat(15));
+    var x = 54;
+    for (var i = 0; i < this.player.hand.length; i++) {
+      for (var j = 0; j < this.player.hand[i].value; j++) {
+        this.display.draw(x, 2, this.player.hand[i].type.symbol, this.player.hand[i].type.color);
+        x += 1;
+      }
+      x += 1;
+    }
   },
 
   _drawMap: function() {
@@ -141,9 +193,9 @@ const Game = {
         this.map[y] = [];
       }
       if (value == 0) {
-        this.map[y][x] = {terrain: Terrain.Room, seen: false};
+        this.map[y][x] = {terrain: Terrain.Room};
       } else {
-        this.map[y][x] = {terrain: Terrain.Rock, seen: false};
+        this.map[y][x] = {terrain: Terrain.Rock};
       }
     };
     digger.create(digCallback.bind(this));
